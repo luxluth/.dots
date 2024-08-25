0c6ed6d188993936a278f068ab44baa9969f2e4f
luxluth <delphin.blehoussi93@gmail.com>
2024-08-25 20:25:45 +0200

    up

---
+++ .config/nvim/lua/plugins/mini-icons.lua
@@ -0,0 +1,9 @@
+return {
+  "echasnovski/mini.icons",
+  opts = {
+    extension = {
+      ["svelte.js"] = { glyph = "", hl = "MiniIconsYellow" },
+      ["svelte.ts"] = { glyph = "", hl = "MiniIconsAzure" },
+    },
+  },
+}
