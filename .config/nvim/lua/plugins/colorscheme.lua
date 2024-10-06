d535858f24124bf3213bb84b0435ef7a35dd0027
luxluth <delphin.blehoussi93@gmail.com>
2024-10-06 22:13:56 +0200

    Moving to gruber-darker

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,13 +1,15 @@
 return {
-  {
-    dir = "/home/luxluth/Code/Projects/japanesque.nvim",
-    priority = 1000,
-    opts = {},
-  },
+  -- {
+  --   dir = "/home/luxluth/Code/Projects/japanesque.nvim",
+  --   priority = 1000,
+  --   opts = {},
+  -- },
+  { "blazkowolf/gruber-darker.nvim", priority = 1000 },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "japanesque",
+      colorscheme = "gruber-darker",
+      -- colorscheme = "japanesque",
     },
   },
 }
