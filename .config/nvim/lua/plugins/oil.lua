bd9db5317d2e14cc73b25c52a336a544ec783f3f
luxluth <delphin.blehoussi93@gmail.com>
2024-12-09 20:37:51 +0100

    oil update

--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -2,7 +2,16 @@ return {
   "stevearc/oil.nvim",
   ---@module 'oil'
   ---@type oil.SetupOpts
-  opts = {},
+  opts = {
+    columns = {
+      "size",
+      "permissions",
+      "mtime",
+      "icon",
+    },
+    skip_confirm_for_simple_edits = true,
+    prompt_save_on_select_new_entry = false,
+  },
   -- Optional dependencies
   dependencies = { { "echasnovski/mini.icons", opts = {} } },
   -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
