a8f3160502a0c1d01b79a791635cc63293629fe0
luxluth <delphin.blehoussi93@gmail.com>
2025-07-28 23:46:52 +0200

    removing dashboard

--- .config/nvim.latest/lua/plugins/disabled.lua
+++ .config/nvim.latest/lua/plugins/disabled.lua
@@ -2,4 +2,5 @@ return {
   { "akinsho/bufferline.nvim", enabled = false },
   { "nvim-neo-tree/neo-tree.nvim", enabled = false },
   { "nvim-lualine/lualine.nvim", enabled = false },
+  { "goolord/alpha-nvim", enabled = false },
 }
