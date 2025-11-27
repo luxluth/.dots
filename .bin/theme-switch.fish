ef5686d630f2bd8e8b294c040d2a218de4909f3d
luxluth <delphin.blehoussi93@gmail.com>
2025-11-27 20:22:13 +0100

    updated theme switch

--- .bin/theme-switch.fish
+++ .bin/theme-switch.fish
@@ -1,13 +1,24 @@
 #!/bin/env fish
 
+function reload_waybar
+    rm ~/.config/waybar/style.css
+    ln -s /home/luxluth/.config/waybar/$argv[1].style.css /home/luxluth/.config/waybar/style.css
+    if pgrep -f waybar >/dev/null
+        killall waybar
+    end
+    waybar &
+end
+
 set current_scheme (gsettings get org.gnome.desktop.interface color-scheme | string replace --all "'" "")
 
 if string match -q default $current_scheme; or string match -q prefer-light $current_scheme
     gsettings set org.gnome.desktop.interface color-scheme prefer-dark
-    kitten themes --reload-in=all "Gruvbox Dark"
     gsettings set org.gnome.desktop.interface gtk-theme adw-gtk3-dark
+
+    reload_waybar dark
 else if string match -q prefer-dark $current_scheme
     gsettings set org.gnome.desktop.interface color-scheme default
-    kitten themes --reload-in=all Dawnfox
     gsettings set org.gnome.desktop.interface gtk-theme adw-gtk3
+
+    reload_waybar light
 end
