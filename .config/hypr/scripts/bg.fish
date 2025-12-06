20bed3bd557181cfba7efb264a78ccea84f63de4
luxluth <delphin.blehoussi93@gmail.com>
2025-12-06 11:28:53 +0100

    update

--- .config/hypr/scripts/bg.fish
+++ .config/hypr/scripts/bg.fish
@@ -1,4 +1,13 @@
 #!/bin/env fish
 
-set image "$HOME/Pictures/walls/wallhaven-gwp3j7.png"
-swww img $image --transition-type none
+set current_scheme (gsettings get org.gnome.desktop.interface color-scheme | string replace --all "'" "")
+
+if pgrep -f swww-daemon >/dev/null
+    if string match -q default $current_scheme; or string match -q prefer-light $current_scheme
+        set image "$HOME/Pictures/walls/wallhaven-gwp3j7.png"
+        swww img $image --transition-type none
+    else
+        set image "$HOME/Pictures/walls/wallhaven-2kpgzm.jpg"
+        swww img $image --transition-type none
+    end
+end
--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,4 +1,4 @@
 return {
-  { "p00f/alabaster.nvim", priority = 1000, config = true },
+  { "luxluth/alabaster.nvim", priority = 1000, config = true },
   { "luxluth/bamboo.nvim", priority = 1000, config = true },
 }
