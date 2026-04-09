0f2c5c8cba23b5c180aff9dc7c4103b9b23d30da
luxluth <delphin.blehoussi93@gmail.com>
2026-04-09 11:01:12 +0200

    up

--- .config/nvim/lua/customs/init.lua
+++ .config/nvim/lua/customs/init.lua
@@ -4,9 +4,12 @@ return {
     local scheme =
       vim.system({ "gsettings", "get", "org.gnome.desktop.interface", "color-scheme" }, { text = true }):wait().stdout
     if scheme ~= nil and scheme:sub(0, scheme:len() - 1) ~= "'prefer-dark'" then
+      vim.o.background = "light"
       vim.cmd("colorscheme alabaster")
+      vim.notify("USING Alabaster", vim.log.levels.WARN, { title = "INIT" })
     else
       vim.cmd("colorscheme bamboo_muted")
+      vim.notify("USING Bamboo", vim.log.levels.WARN, { title = "INIT" })
     end
   end,
 }
