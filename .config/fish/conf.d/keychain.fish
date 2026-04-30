fdbd8b6a2eb43a210884f5691b1261667cdd5848
luxluth <delphin.blehoussi93@gmail.com>
2026-04-30 07:56:48 +0200

    fish: check if i can reach the ssh-agent before running eval

--- .config/fish/conf.d/keychain.fish
+++ .config/fish/conf.d/keychain.fish
@@ -1,13 +1,8 @@
 if status is-interactive
     set -g SSH_KEYS_TO_AUTOLOAD github vpshelha
 
-    # Check if the keychain file exists and source it directly
-    if test -f ~/.keychain/(hostnamectl hostname)-fish
-        source ~/.keychain/(hostnamectl hostname)-fish
-    end
-
     # Only run the full keychain command if the socket is missing (first login)
-    if not set -q SSH_AUTH_SOCK
-        eval (keychain --eval --quiet $SSH_KEYS_TO_AUTOLOAD)
+    if not ssh-add -l >/dev/null 2>&1
+        eval (keychain --eval $SSH_KEYS_TO_AUTOLOAD)
     end
 end
